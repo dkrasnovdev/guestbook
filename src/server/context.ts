84de0e9ca35eacd863625cc46e61f1c96c3403bd
Dmtitry Krasnov <dkrasnov.dev@gmail.com>
2023-09-20 21:14:21 +1200

    (fix): import trpc & trpcNext as types

--- src/server/context.ts
+++ src/server/context.ts
@@ -1,5 +1,5 @@
-import * as trpc from "@trpc/server";
-import * as trpcNext from "@trpc/server/adapters/next";
+import type * as trpc from "@trpc/server";
+import type * as trpcNext from "@trpc/server/adapters/next";
 import { getServerSession } from "next-auth";
 import { authOptions } from "~/pages/api/auth/[...nextauth]";
 
