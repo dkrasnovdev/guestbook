971b83815d195fb0439887f3bc0f0e5e7e86aece
Dmtitry Krasnov <dkrasnov.dev@gmail.com>
2023-09-21 18:39:07 +1200

    (feat): opt-out to using drizzle-kit to push migrations

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,14 +1,14 @@
-import type { Config } from 'drizzle-kit'
+import type { Config } from "drizzle-kit";
 
 if (!process.env.DATABASE_URL) {
-  throw new Error('DATABASE_URL is missing');
+  throw new Error("DATABASE_URL is missing");
 }
 
 export default {
-  schema: './src/server/db/schema.ts',
-  out: './src/server/db/migrations',
+  driver: "pg",
+  schema: "./src/server/db/schema.ts",
+  out: "./src/server/db/migrations",
   dbCredentials: {
     connectionString: process.env.DATABASE_URL,
   },
-  breakpoints: true,
-} satisfies Config
+} satisfies Config;
